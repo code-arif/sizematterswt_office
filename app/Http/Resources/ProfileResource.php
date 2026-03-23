aa11f8df19502ababf2ca704c4da31f880339941
code-arif <arifulislam6460@gmail.com>
2026-03-23 09:57:45 +0600

    add full url for profile image

--- app/Http/Resources/ProfileResource.php
+++ app/Http/Resources/ProfileResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileResource extends JsonResource
 {
@@ -14,7 +15,7 @@ class ProfileResource extends JsonResource
             'name' => $this->name ?? '',
             'username' => $this->username ?? '',
             'avatar' => $this->avatar
-                ? asset($this->avatar)
+                ? url(Storage::url($this->avatar))
                 : asset('admin/default/user.jpg'),
         ];
     }
